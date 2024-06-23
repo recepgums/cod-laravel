f3fb8f2fc9f0068d7a9b12946a0f022bd1d9fbcd
Recep G <recep.gumus@yunggroup.com>
2024-06-23 22:29:30 +0300

    uzay bulut robot

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Helpers\WhatsappHelper;
 use App\Models\Order;
 
 class OrderObserver
@@ -20,7 +21,7 @@ class OrderObserver
     public function updated(Order $order): void
     {
         if (!$order->getOriginal('is_done') && $order->is_done) {
-
+            WhatsappHelper::sendMessage($order->phone,"Merhaba ". $order->name . ", \n\n Siparişinizi aldık: \n\n". $order->products);
         }
     }
 
